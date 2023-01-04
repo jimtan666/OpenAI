58b641a0088e66a216538f8f8ecf6e7c38412644
jimtan666 <39551317+jimtan666@users.noreply.github.com>
2023-01-05 00:47:44 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from base_utils import *
+from middle_utils import *
+from high_utils import *
+
+
+class OpenCV(object):
+    def __init__(self):
+        pass
+
+    def distance_point_line(self):
+        pass
+
+    def distance_two_point(self):
+        pass
+
